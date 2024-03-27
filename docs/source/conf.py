042389c470c99e9fb4189887a56f01f38d2c0775
NRajaL <rajalekshmin@evolbio.mpg.de>
2024-03-27 15:41:33 +0100

    ./docs/source/conf.py renamed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,16 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Python Template Package'
+project = 'Python Hamming Package'
 copyright = '2024, Max Planck Institute for Evolutionary Biology'
 author = 'Max Planck'
 version = "version = "version = "version = "version = "0.0.1"""""
 
 import sys
 
-sys.path.insert(0, '../../template')
+sys.path.insert(0, '../../hamming')
 
-import template
+import hamming
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,7 +26,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               ]
 
-templates_path = ['_templates']
+hammings_path = ['_hammings']
 exclude_patterns = []
 
 source_suffix = ['.rst', '.md']
